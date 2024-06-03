8ab394d88a639ca96e1f540abbb9e761242131ad
Marc Bria <marc.bria@uab.es>
2024-06-03 12:31:11 +0200

    Adding deleteJournal. Fixing permissions for getRemote

---
+++ deleteJournal.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Check if a journal name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "REMOVES all data for the specified journal"
+    echo "ERROR: Missing argument with journal's name."
+    echo "Usage: $0 <journal>"
+    exit 1
+fi
+
+journal=${1}
+
+# Verify if is running with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "This script needs sudo privileges. Run as follows 'sudo ./deleteJournal.sh'"
+    exit 1
+fi
+
+################################
+## CONFIG VARIABLES
+################################
+
+# Setting local folders (destination):
+journalFiles=/srv/volumes/files
+
+# Variables commented (not set) will be ignored.
+journalSite=/home/docker/sites/${journal}
+journalDbDumps=/srv/backups/migration/${journal}
+journalAll=/srv/volumes/all/${journal}
+journalDb=/srv/volumes/db/${journal}
+journalLogs=/srv/volumes/logs/${journal}
+journalFilesPublic=${journalFiles}/public/${journal}
+journalFilesPrivate=${journalFiles}/private/${journal}
+journalFilesConfigs=${journalFiles}/config/old/${journal}
+
+
+################################
+## Backup all data.
+################################
+
+# Stops and removes the containers
+cd $journalSite
+docker compose stop
+docker compose down
+
+# Create a tarball of ALL data for this journal named YYYYMMDD-HHMM-journal.tgz
+timestamp=$(date +%Y%m%d-%H%M)
+backupFile="/srv/backups/${journal}/${timestamp}-${journal}.tgz"
+
+tar -czf $backupFile $journalSite $journalDbDumps $journalAll $journalDb $journalLogs $journalFilesPublic $journalFilesPrivate $journalFilesConfigs
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Backup creation failed!"
+    exit 1
+fi
+
+echo "Backup created at $backupFile"
+
+# Warning message
+read -p "This will REMOVE all data for $journal. Are you sure you want to continue? [y/N] " confirm
+if [[ $confirm != [yY] ]]; then
+    echo "Operation cancelled."
+    exit 1
+fi
+
+# Check if $journal folders are defined AND exists before deleting
+folders=($journalDbDumps $journalAll $journalDb $journalLogs $journalFilesPublic $journalFilesPrivate $journalFilesConfigs)
+
+for folder in "${folders[@]}"; do
+    if [ ! -d "$folder" ]; then
+        echo "ERROR: Folder $folder does not exist."
+        exit 1
+    fi
+done
+
+# Removes the specified folders
+rm -rf $journalSite
+rm -rf $journalDbDumps
+rm -rf $journalDb
+rm -rf $journalLogs
+rm -rf $journalFilesPublic
+rm -rf $journalFilesPrivate
+rm -rf $journalFilesConfigs
+rm -rf $journalAll
+
+echo ">>>> ALL data for $journal was REMOVED!"
--- getRemoteVol.sh
+++ getRemoteVol.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Check if a journal name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Pulls all the volumes of the specified journal from the remote server"
+    echo "ERROR: Missing argument with journal's name."
+    echo "Usage: $0 <journal>"
+    exit 1
+fi
+
 journal=${1:-"demo"}
 
-echo "Gets the volumes of the specified journal from the remote server"
+echo "Gets all the volumes of the specified journal from the remote server"
 
 # Verificar si se ejecuta con sudo
 if [ "$EUID" -ne 0 ]; then
@@ -12,15 +20,19 @@ fi
 
 startTime=$(date +%s)
 
+################################
 ## CONFIG VARIABLES
+################################
 
 # Remote user (need the proper privileges in the remote machine)
 remoteUsr=marc
 
+# IMPORTANT: Before running the script you need to exchange the keys.
+# After this, indicate where is your private key to rsync without pwds.
 privateKeyPath=/home/${remoteUsr}/.ssh/id_rsa
 
-# Setting local folders:
-srcBackups=/srv/backup/duplicati/db-dump/*${journal}*
+# Setting remote folders (source)
+srcDbDumps=/srv/backup/duplicati/db-dump/*${journal}*
 srcAll=/srv/volumes/${journal}
 srcDb=/srv/volumes/${journal}/db
 srcLogs=/srv/volumes/${journal}/logs
@@ -29,17 +41,25 @@ srcFilesPublic=/srv/volumes/${journal}/public
 srcFilesPrivate=/srv/volumes/${journal}/private
 srcFilesConfigs=/srv/volumes/${journal}/config
 
-destBackups=/srv/backups/${journal}
+# Setting local folders (destination):
+destFiles=/srv/volumes/files 			
+
+# Variables commented (not set) will be ignored.
+destDbDumps=/srv/backups/migration/${journal}
 destAll=/srv/volumes/all/${journal}
-destDb=/srv/volumes/db/${journal}
+# destDb=/srv/volumes/db/${journal}
 destLogs=/srv/volumes/logs/${journal}
-destFiles=/srv/volumes/files
 destFilesPublic=${destFiles}/public/${journal}
 destFilesPrivate=${destFiles}/private/${journal}
-destFilesConfigs=${destFiles}/config/${journal}
+destFilesConfigs=${destFiles}/config/old/${journal}
+
+
+################################
+## Running the data move
+################################
 
 # Debug:
-## rm $destBackups/${journal} -Rf
+## rm $destDbDumps/${journal} -Rf
 ## rm $destDb/${journal} -Rf
 ## rm $destLogs/${journal} -Rf
 ## rm $destFiles/${journal} -Rf
@@ -47,58 +67,94 @@ destFilesConfigs=${destFiles}/config/${journal}
 ## rm $destAll/${journal} -Rf
 
 echo "Ensures the required folder structure exists"
-mkdir ${destBackups}        > /dev/null 2>&1
-mkdir ${destAll}            > /dev/null 2>&1
-mkdir ${destDb}             > /dev/null 2>&1
-mkdir ${destLogs}           > /dev/null 2>&1
-mkdir ${destFiles}          > /dev/null 2>&1
-mkdir ${destFilesPublic}    > /dev/null 2>&1
-mkdir ${destFilesPrivate}   > /dev/null 2>&1
-mkdir ${destFilesConfigs}   > /dev/null 2>&1
+mkdir -p ${destDbDumps}        > /dev/null 2>&1
+mkdir -p ${destAll}            > /dev/null 2>&1
+mkdir -p ${destDb}             > /dev/null 2>&1
+mkdir -p ${destLogs}           > /dev/null 2>&1
+mkdir -p ${destFiles}          > /dev/null 2>&1
+mkdir -p ${destFilesPublic}    > /dev/null 2>&1
+mkdir -p ${destFilesPrivate}   > /dev/null 2>&1
+mkdir -p ${destFilesConfigs}   > /dev/null 2>&1
 
-# Pulls the DB dumps and place them on migration folder
-# Notice:
-# Locally this script need to be sudoer. Not asked here because the script is.
-# Remotelly rsync needs sudo to reach all files. rsync-path parameter adds sudo privileges.
-# We need to exchange keys to avoid asking passwords all the time.
-# Final slash in srcVars indicate we like to copy the content (not the folder).
+# Commons folder: Run once
+# rsync -avzh -e "ssh -i /home/marc/.ssh/id_rsa" --rsync-path="sudo rsync" marc@adacar:/home/dojo/sites/common/ /home/docker/sites/common -a
 
-echo "--> Rsyncing: ${srcBackups}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcBackups} ${destBackups}
-unlink ${destAll}/migration
-ln -s ${destBackups} ${destAll}/migration
+
+# Pulls DB Dumps
+if [ -n "$destDbDumps" ]; then
+	echo "--> Rsyncing: ${srcDbDumps}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcDbDumps} ${destDbDumps}
+	unlink ${destAll}/migration
+	ln -s ${destDbDumps} ${destAll}/migration
+	# Removes al files except the newer one:
+    cd ${destDbDumps}
+    ls -t | head -n 2 | tail -n 1 | xargs rm -f
+    # Sets permissions for mysql:mysql user and group
+    chown 999:999 ${destDbDumps}/ -Rh
+    find ${destDbDumps} -type d -exec chmod 0755 {} \;
+    find ${destDbDumps} -type f -exec chmod 0755 {} \;
+fi
 
 # Pulls the db/Log/files
-echo "--> Rsyncing: ${srcDb}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcDb}/ ${destDb}
-unlink ${destAll}/db
-ln -s ${destDb} ${destAll}/db
+if [ -n "$destDb" ]; then
+	echo "--> Rsyncing: ${srcDb}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcDb}/ ${destDb}
+	unlink ${destAll}/db
+	ln -s ${destDb} ${destAll}/db
+    # Sets permissions for mysql:mysql user and group
+    chown 999:999 ${destAll}/db/ -R
+    find ${destAll}/db -type d -exec chmod 0755 {} \;
+    find ${destAll}/db -type f -exec chmod 0644 {} \;
+fi
 
-echo "--> Rsyncing ${srcLogs}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcLogs}/ ${destLogs}
-unlink ${destAll}/logs
-ln -s ${destLogs} ${destAll}/logs
+# Pulls the logs
+if [ -n "$destLogs" ]; then
+	echo "--> Rsyncing ${srcLogs}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcLogs}/ ${destLogs}
+	unlink ${destAll}/logs
+	ln -s ${destLogs} ${destAll}/logs
+    # Sets permissions for apache:apache user and group
+    chown 100:101 ${destAll}/logs/ -R
+    find ${destAll}/logs -type d -exec chmod 0755 {} \;
+    find ${destAll}/logs -type f -exec chmod 0644 {} \;
+fi
 
-echo "--> Rsyncing ${srcFilesPublic}"
-echo "rsync -avzh -e \"ssh -i ${privateKeyPath}\" --rsync-path=\"sudo rsync\" ${remoteUsr}@adacar:${srcFilesPublic}/ ${destFilesPublic}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesPublic}/ ${destFilesPublic}
-unlink ${destAll}/public
-ln -s ${destFilesPublic} ${destAll}/public
+# Pulls the Public Files
+if [ -n "$destFilesPublic" ]; then
+	echo "--> Rsyncing ${srcFilesPublic}"
+	echo "rsync -avzh -e \"ssh -i ${privateKeyPath}\" --rsync-path=\"sudo rsync\" ${remoteUsr}@adacar:${srcFilesPublic}/ ${destFilesPublic}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesPublic}/ ${destFilesPublic}
+	unlink ${destAll}/public
+	ln -s ${destFilesPublic} ${destAll}/public
+    chown 100:101 ${destAll}/public/ -Rh
+    chmod 750 ${destAll}/public/ -Rh
+    find ${destAll}/public -type d -exec chmod 0755 {} \;
+    find ${destAll}/public -type f -exec chmod 0644 {} \;
+fi
 
-echo "--> Rsyncing ${srcFilesPrivate}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesPrivate}/ ${destFilesPrivate}
-unlink ${destAll}/private
-ln -s ${destFilesPrivate} ${destAll}/private
+# Pulls the Private Files
+if [ -n "$destFilesPrivate" ]; then
+	echo "--> Rsyncing ${srcFilesPrivate}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesPrivate}/ ${destFilesPrivate}
+	unlink ${destAll}/private
+	ln -s ${destFilesPrivate} ${destAll}/private
+    chown 100:101 ${destAll}/private/ -R
+    find ${destAll}/private -type d -exec chmod 0750 {} \;
+    find ${destAll}/private -type f -exec chmod 0640 {} \;
+fi
 
-echo "--> Rsyncing ${srcFilesConfig}"
-rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesConfigs}/ ${destFilesConfigs}
-unlink ${destAll}/config
-ln -s ${destFilesConfigs} ${destAll}/config
+# Pulls the Config Files
+if [ -n "$destFilesPrivate" ]; then
+	echo "--> Rsyncing ${srcFilesConfig}"
+	rsync -avzh -e "ssh -i ${privateKeyPath}" --rsync-path="sudo rsync" ${remoteUsr}@adacar:${srcFilesConfigs}/ ${destFilesConfigs}
+	# unlink ${destAll}/config
+	# ln -s ${destFilesConfigs} ${destAll}/config
+fi
 
 endTime=$(date +%s)
 
-# Calcula el tiempo transcurrido
+# Calculates the elapsed time
 elapsedTime=$((endTime - startTime))
 formattedTime=$(date -u -d @"$elapsedTime" +'%H:%M:%S')
 
-echo ">>>> La còpia de los volúmenes de $journal ha tardado: $formattedTime"
+echo ">>>> The transfer of the volumes of $journal took: $formattedTime"
